3962a3163d0220478c345bbf692fd9c88f4464b5
pianoHandu <pianoHand@dujiayundeMacBook-Pro.local>
2015-07-20 16:10:31 +0800

    汣零

---
+++ 汣零/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  汣零
+//
+//  Created by 杜佳云 on 15/7/20.
+//  Copyright (c) 2015年 pianoHand. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 汣零/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  汣零
+//
+//  Created by 杜佳云 on 15/7/20.
+//  Copyright (c) 2015年 pianoHand. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
